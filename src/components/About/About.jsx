404046894d2261d4f0fee316ad652b44950bc382
Mayank Gaur <mayankgaur1504@gmail.com>
2025-06-18 02:01:46 +0530

    resume updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1jSEwJendKRaj7dCTP_3PcopKScbmxgOG/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1rxQx47M9Vyyj3m9cmkOsib27op4EvDIk/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
