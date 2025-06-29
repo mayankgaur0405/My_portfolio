7b5174d6ca0326a4e86d91f4b342b85a0f0a35a7
Mayank Gaur <mayankgaur1504@gmail.com>
2025-06-29 18:05:38 +0530

    updated cv drive link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/186exkDJEdTTJqjdd2wInW2hZy9aUTxdx/view?usp=sharing"
+            href="https://drive.google.com/file/d/1qHKrM6vrrXi12dV-K_WilUOQPRjWPfMD/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
